74e0fb254e68f7984f95d81a837481105d39c988
Luandro <luandro@gmail.com>
2015-08-26 16:40:34 -0300

    fixed some casks

---
+++ BasicMac.sh
@@ -0,0 +1,162 @@
+####################### INSTALL BASIC STUFF ################
+
+# TODO: Clone VVV, VV from Github; Create directory and add my projects; Automatically change .zshrc
+
+#Install Brew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#Update
+brew update
+
+# homebrew taps
+brew tap caskroom/cask
+brew tap caskroom/versions
+brew tap homebrew/dupes
+brew tap caskroom/versions
+brew tap homebrew/boneyard
+brew tap caskroom/fonts
+
+# Updated grep
+brew install grep
+
+# Updated curl
+brew install curl
+brew link curl --force
+
+# Updated bash
+brew install bash
+
+# Updated python/pip
+brew install python
+pip install --upgrade setuptools
+pip install --upgrade pip
+
+# homebrew binaries
+binaries=(
+	caskroom/cask/brew-cask
+	graphicsmagick
+	mackup
+	wget
+	youtube-dl
+	webkit2png
+	rename
+	zopfli
+	ffmpeg
+	python
+	trash
+	node
+	tree
+	ack
+	hub
+	git
+	git-extras
+)
+
+echo "installing binaries..."
+brew install ${binaries[@]}
+
+# cask apps
+apps=(
+	spotify
+	utorrent
+	screenflick
+	hazel 
+	sublime-text3
+	vlc
+	vagrant
+	virtualbox
+	android-file-transfer
+	slack
+	appcleaner
+	firefox
+	sketch
+	skype
+	iterm2
+	sourcetree
+	alfred
+	google-chrome
+	cyberduck
+	google-hangouts
+	dash
+	xquartz
+	the-unarchiver
+	lastpass
+	paparazzi
+	bartender
+	caffeine
+	sketch-toolbox
+	cleanmymac
+	imageoptim
+	macdown
+	qlcolorcode 
+	qlstephen 
+	qlmarkdown 
+	quicklook-json 
+	qlprettypatch
+	quicklook-csv
+	betterzipql
+	qlimagesize
+	webpquicklook
+	suspicious-package
+)
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo "installing apps..."
+brew cask install --appdir="/Applications" ${apps[@]}
+
+# Vagrant plugins
+vagrant plugin install vagrant-hostsupdater
+vagrant plugin install vagrant-triggers
+
+# Install fonts
+fonts=(
+	font-lato
+	font-ubuntu
+	font-roboto
+	font-raleway font-raleway-dots
+	font-bebas-neue
+	font-meslo-lg-for-powerline
+)
+
+echo "installing fonts..."
+brew cask install ${fonts[@]}
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# NPM
+node_packages=(
+  	yo
+  	gulp
+  	json-server
+  	nodemon
+  	pm2
+  	babel
+  	webpack
+  	grunt
+  	bower
+)
+echo "installing node packages..."
+npm install -g  ${node_packages[@]}
+
+###############################################################################
+# Sublime Text
+############################################################################### 
+echo "Setting Git to use Sublime Text as default editor"
+git config --global core.editor "subl -n -w"
+
+# Oh My Zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+zsh --version
+chsh -s /bin/zsh
+
+# The Ultimate Vim Distribution
+curl http://j.mp/spf13-vim3 -L -o - | sh
+
+# verify
+brew doctor
+
+# Cleanup
+brew cleanup
---
+++ README.md
@@ -0,0 +1,74 @@
+#My personal initial config
+
+##OSx options
+
+##Apps
+- [Brew](http://brew.sh/) ```ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"```
+
+- [Brew cask](http://caskroom.io/) ```brew install cask```
+
+- [WGET](http://www.gnu.org/software/wget/) ``` brew install wget```
+
+- [youtube-dl](https://rg3.github.io/youtube-dl/) ```brew install youtube-dl```
+
+- [Spotify](https://www.spotify.com/br/) ```brew cask install spotify```
+
+- [VLC](http://www.videolan.org/vlc/) ```brew cask install vlc```
+
+- [uTorrent]() ```brew cask install utorrent```
+
+- [Google Chrome](https://www.google.com.br/chrome/browser/desktop/) ```brew cask install google-chrome```
+
+####Fonts
+```
+brew tap caskroom/fonts
+brew cask install font-lato
+font-ubuntu
+font-roboto
+font-raleway font-raleway-dots
+font-bebas-neue
+```
+
+
+####[Sublime Text 3](http://google.com)
+```
+brew tap caskroom/versions
+
+brew cask install sublime-text3
+```
+
+- [Emmet](http://google.com) with [JSX](http://google.com) support
+
+####[iTerm2](http://webpack.github.io/)
+```brew cask install iterm2```
+
+- [Dark Solarized](http://google.com) color scheme
+- [Meslo Powerline icon](http://google.com) for non ASCII characters
+
+##CLI
+
+####[Zsh](http://google.com)
+- [Oh-my-Zsh](http://ohmyz.sh/) with _agnoster_ theme and plugins=(sublime git autojump brew node npm sudo tmux tmuxinator vagrant web-search) 
+- ZSH_THEME="agnoster"
+- plugins=(sublime git autojump brew node npm sudo tmux tmuxinator vagrant web-search)
+
+
+- [Z](https://github.com/rupa/z) to jump around
+
+####[Vim](http://google.com)
+- [spf13-vim](http://vim.spf13.com/)
+
+##NodeJs
+- [json-server](https://github.com/typicode/json-server)
+- [nodemon](http://google.com)
+- [gulp](http://google.com)
+- [pm2](http://google.com)
+- [babel](http://google.com)
+- [webpack]()
+- [grunt]()
+- [yo]()
+- [bower]()
+
+####Vagrant
+- [VVV](https://github.com/varying-vagrant-vagrants/vvv) - Varying Vagrant Vagrants
+- [VV - Site Wizard](https://github.com/bradp/vv)
