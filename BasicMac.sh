fc99bba6dd97fe16b4a741812bf0f8758462edfb
luandro <luandro@gmail.com>
2017-08-02 14:23:36 -0300

    update

--- BasicMac.sh
+++ BasicMac.sh
@@ -1,12 +1,21 @@
 ####################### INSTALL BASIC STUFF ################
 
-# TODO: Clone VVV, VV from Github; Create directory and add my projects; Automatically change .zshrc
+# Ask for the administrator password upfront.
+sudo -v
 
-#Install Brew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 #Update
 brew update
+brew upgrade --all
 
 # homebrew taps
 brew tap caskroom/cask
@@ -26,6 +35,14 @@ brew link curl --force
 
 # Updated bash
 brew install bash
+brew tap homebrew/versions
+brew install bash-completion2
+echo "Adding the newly installed shell to the list of allowed shells"
+# Prompts for password
+sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
+# Change to the new shell, prompts for password
+chsh -s /usr/local/bin/bash
+brew install wget --with-iri
 
 # Updated python/pip
 brew install python
@@ -43,6 +60,27 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install node
 
+#install YARN
+npm i -g yarn
+
+# Install packages
+node_packages=(
+  	yo
+	fixpack
+	standard
+	gh-pages-deploy
+  	webpack
+  	nodemon
+  	pm2
+	np
+	create-react-app
+	live-server
+	truffle
+	ethereumjs-testrpc
+)
+echo "installing node packages..."
+yarn global add  ${node_packages[@]}
+
 # homebrew binaries
 binaries=(
 	caskroom/cask/brew-cask
@@ -50,12 +88,8 @@ binaries=(
     heroku-toolbelt
     postgresql
     mongodb
-    ghc 
-    cabal-install
-    sqitch_pg
 	graphicsmagick
 	mackup
-	wget
 	youtube-dl
 	webkit2png
 	rename
@@ -84,12 +118,10 @@ apps=(
 	visual-studio-code
 	vlc
 	android-file-transfer
-	slack
 	appcleaner
 	firefox
 	sketch
 	iterm2
-	sourcetree
 	alfred
 	google-chrome
 	cyberduck
@@ -132,33 +164,19 @@ brew cask install ${fonts[@]}
 
 # Install GNU core utilities (those that come with OS X are outdated)
 brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+brew install moreutils
+brew install gnu-sed --with-default-names
+
+
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
 brew install findutils
 
 #Sass
 sudo gem install sass
 
-# NPM
-node_packages=(
-	yarn
-  	yo
-	fixpack
-	standard
-	gh-pages-deploy
-  	webpack
-  	nodemon
-  	pm2
-	np
-	create-react-app
-	live-server
-	truffle
-	ethereumjs-testrpc
-)
-echo "installing node packages..."
-npm install -g  ${node_packages[@]}
-
 ###############################################################################
-# Sublime Text
+# Visual Code Studio
 ############################################################################### 
 echo "Setting Git to use Visual Code Studio as default editor"
 git config --global core.editor "code -w"
@@ -168,8 +186,42 @@ curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | 
 zsh --version
 chsh -s /bin/zsh
 
+# Remove plugins
+grep -v "plugins=(git)" ~/.zshrc > ~/.zshrc2; mv ~/.zshrc2 ~/.zshrc
+grep -v 'ZSH_THEME="robbyrussell"' ~/.zshrc > ~/.zshrc2; mv ~/.zshrc2 ~/.zshrc
+
+addToZshrc=(
+	ZSH_THEME="agnoster"
+	plugins=(git autojump brew node npm sudo lighthouse web-search osx)
+	export NVM_DIR=~/.nvm
+	source ~/.nvm/nvm.sh 
+	export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/git/bin"
+	export SSH_KEY_PATH="~/.ssh/rsa_id"
+	export EDITOR='mvim'
+	alias zshconfig="vim ~/.zshrc"
+	alias ohmyzsh="vim  ~/.oh-my-zsh"
+	alias v="mvim -v"
+	export PATH="/usr/local/sbin:$PATH"
+
+)
+
+echo ${addToZshrc[@]} >> ~/.zshrc
+
+# Z
+wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O ~/z.sh
+echo . ~/z.sh >> ~/.zshrc
+
+
 # The Ultimate Vim Distribution
 curl http://j.mp/spf13-vim3 -L -o - | sh
+
+# Git & SSH
+git credential-osxkeychain
+git config --global credential.helper osxkeychain
+ssh-keygen -t rsa -b 4096 -C "luandro@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+chromium-browser https://github.com/settings/keys
 
 # verify
 brew doctor
